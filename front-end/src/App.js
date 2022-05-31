a00ebfa8967e103751b72f5eb540cdbcfbf03baf
vinicius silva <vinicius_dev@hotmail.com>
2022-05-31 18:31:38 -0300

    feat: rotas no app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Login from './pages/Login'
-import Register from './pages/Register';
 import { Routes, Route } from 'react-router-dom';
-
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (

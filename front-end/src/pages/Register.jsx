18d5e2f77c100fe23f819f5b30736fbcd7e30a92
vinicius silva <vinicius_dev@hotmail.com>
2022-05-31 18:29:10 -0300

    feat: Cria esboco pagina `register`

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+      <>
+        <div>
+            <h2>Register</h2>
+        </div>
+    </>
+  );
+};
+
+export default Register;

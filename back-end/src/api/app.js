78be58f87c6b297913107c150f10532bb8d5d905
Jesse Benevides <jessebenevides@gmail.com>
2022-06-01 17:09:41 -0300

    feat(API): add LoginRouter ao App

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { loginRouter } = require('../routes/loginRoutes');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/login', loginRouter);
 
 module.exports = app;
 

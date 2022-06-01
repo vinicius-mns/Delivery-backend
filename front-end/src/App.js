50931ff82adc2b622e3cad95b344ae6b71606109
Leandro <leandrooliveira35@gmail.com>
2022-06-01 19:20:07 -0300

    feat(front): Cria rota customer/products

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Products from './pages/Products';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
       <Route path="/register" element={ <Register /> } />
+      <Route path="/customer/products" element={ <Products /> } />
     </Routes>
   );
 }

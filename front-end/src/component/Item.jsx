9d72ff3be6a4f5b27dbc6cf7398d65cb517ca687
Leandro <leandrooliveira35@gmail.com>
2022-06-01 17:40:15 -0300

    feat: generate prop-types

--- front-end/src/component/Item.jsx
+++ front-end/src/component/Item.jsx
@@ -1,16 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-const Item = ({ title, testId, type, handlechand }) => {
-  return (
-    <div className="item">
-      <h2>{title}</h2>
-      <input
-        type={ type }
-        data-testid={ testId }
-        onChange={ handlechand }
-      />
-    </div>
-  );
+const Item = ({ title, testId, type, handleChange }) => (
+  <div className="item">
+    <h2>{title}</h2>
+    <input type={ type } data-testid={ testId } onChange={ handleChange } />
+  </div>
+);
+
+Item.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  testId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default Item;

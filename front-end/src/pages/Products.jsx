667a20fb99bf163a6b0ad9ef94ec8a3f7da6a74d
vinicius silva <vinicius_dev@hotmail.com>
2022-06-03 03:16:51 -0300

    feat(front): Cria mock da pagina de produtos

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,8 +1,97 @@
 import React from 'react';
+import Card from '../component/card';
+import '../styles/bar.css';
+
+const sair = 'https://cdn-icons-png.flaticon.com/512/63/63769.png';
 
 const Products = () => (
   <div>
-    <h1>Toma-lhe teu produto</h1>
+    <div className="segredo">
+      <button type="button" className="name"><h2>Vinicius Marcelino Silva</h2></button>
+      <div className="bar">
+        <button type="button">Produtos</button>
+        <button type="button">Meus pedidos</button>
+        <button type="button" className="carrinho">Ver Carrinho: R$: 34:99</button>
+      </div>
+      <button type="button" className="sair">Sair</button>
+    </div>
+    <div className="container2">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+    </div>
   </div>
 );
 

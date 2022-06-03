0f64f07b512c750a8c4d6cb92603e70b89f68b06
Jesse Benevides <jessebenevides@gmail.com>
2022-06-03 15:35:26 -0300

    feat(front): add requisição de produtos à API

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,27 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../component/card';
 import '../styles/bar.css';
 import logo from '../images/logo.png';
+import { requestGet } from '../service/request';
 
-const Products = () => (
-  <div className="products">
-    <img src={ logo } alt="logo" className="backgroundImage" />
-    <div className="segredo">
-      <button type="button" className="name"><h2>Vinicius Marcelino Silva</h2></button>
-      <div className="bar">
-        <button type="button">Produtos</button>
-        <button type="button">Meus pedidos</button>
-        <button type="button" className="carrinho">Ver Carrinho: R$: 34:99</button>
+const Products = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const getProduts = async () => {
+      const allProducts = await requestGet('/products');
+      setProductList(allProducts);
+    };
+
+    getProduts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="products">
+      <img src={ logo } alt="logo" className="backgroundImage" />
+      <div className="segredo">
+        <button type="button" className="name">
+          <h2>Vinicius Marcelino Silva</h2>
+        </button>
+        <div className="bar">
+          <button type="button">Produtos</button>
+          <button type="button">Meus pedidos</button>
+          <button type="button" className="carrinho">
+            Ver Carrinho: R$: 34:99
+          </button>
+        </div>
+        <button type="button" className="sair">
+          Sair
+        </button>
       </div>
-      <button type="button" className="sair">Sair</button>
+      <div className="container2">
+        {
+          productList.map(({ urlImage, id, name, price }) => (
+            <Card name={ name } key={ id } price={ price } img={ urlImage } />
+          ))
+        }
+      </div>
+      <div className="showLogo" />
     </div>
-    <div className="container2">
-      {mock.map(({ img, id, name, price }) => {
-        <Card name={ name } key={ id } price={ price } img={ img } />;
-      })}
-    </div>
-    <div className="showLogo" />
-  </div>
-);
+  );
+};
 
 export default Products;

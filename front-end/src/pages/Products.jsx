774144a2530c6a2e258fddb6f7285d3c56bc4e3f
Jesse Benevides <jessebenevides@gmail.com>
2022-06-03 19:24:57 -0300

    fix(front): add testIds nos elementos da page Products

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -22,16 +22,37 @@ const Products = () => {
       <img src={ logo } alt="logo" className="backgroundImage" />
       <div className="segredo">
         <button type="button" className="name">
-          <h2>Vinicius Marcelino Silva</h2>
+          <h2 data-testid="customer_products__element-navbar-user-full-name">
+            { user.name }
+          </h2>
         </button>
         <div className="bar">
-          <button type="button">Produtos</button>
-          <button type="button">Meus pedidos</button>
-          <button type="button" className="carrinho">
+          <button
+            data-testid="customer_products__element-navbar-link-products"
+            type="button"
+          >
+            Produtos
+          </button>
+          <button
+            data-testid="customer_products__element-navbar-link-orders"
+            type="button"
+          >
+            Meus pedidos
+          </button>
+          <button
+            data-testid="customer_products__checkout-bottom-value"
+            type="button"
+            className="carrinho"
+          >
             Ver Carrinho: R$: 34:99
           </button>
         </div>
-        <button type="button" className="sair">
+        <button
+          data-testid="customer_products__element-navbar-link-logout"
+          type="button"
+          className="sair"
+          onClick={ logout }
+        >
           Sair
         </button>
       </div>

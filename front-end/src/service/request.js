4c21800a61fdf3892a162798261a6ce652557b7f
Jesse Benevides <jessebenevides@gmail.com>
2022-06-03 15:34:56 -0300

    feat(front): add função resquetGet

--- front-end/src/service/request.js
+++ front-end/src/service/request.js
@@ -10,4 +10,10 @@ export const requestPost = async (endpoint, body) => {
   return data;
 };
 
+export const requestGet = async (endpoint) => {
+  const { data } = await api.get(endpoint);
+
+  return data;
+};
+
 export default api;

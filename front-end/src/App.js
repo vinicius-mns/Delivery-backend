85761b65f2f532bb14a526f933407d79975ead6f
Leandro <leandrooliveira35@gmail.com>
2022-06-04 22:07:15 -0300

    feat(front): retira o footer das paginas login e registro

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
+import AdminManager from './pages/AdminManager';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
 import SellerOrders from './pages/SellerOrders';
-import AdminManager from './pages/AdminManager';
 
 function App() {
+  const location = useLocation();
   return (
     <div>
-
       <Routes>
         <Route path="/login" element={ <Login /> } />
         <Route path="/register" element={ <Register /> } />
@@ -19,7 +19,9 @@ function App() {
         <Route path="/admin/manage" element={ <AdminManager /> } />
         <Route exact path="/" element={ <Navigate to="/login" /> } />
       </Routes>
-      <footer>© Ostron company Todos direitos reservados</footer>
+      {(location.pathname !== '/login' && location.pathname !== '/register') && (
+        <footer>© Ostron company Todos direitos reservados</footer>
+      ) }
     </div>
   );
 }

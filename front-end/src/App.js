21de9fc829bfb0344c249aad163d5a8a934e13c5
vinicius silva <vinicius_dev@hotmail.com>
2022-06-05 19:37:55 -0300

    refactor(front): Implementa componente Routers no app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
-import Products from './pages/Products';
-import Register from './pages/Register';
-import SellerOrders from './pages/SellerOrders';
-import AdminManager from './pages/AdminManager';
-import * as path from './utils/paths';
+import Routers from './component/Routers';
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path={ path.login } element={ <Login /> } />
-        <Route path={ path.registe } element={ <Register /> } />
-        <Route path={ path.product } element={ <Products /> } />
-        <Route path={ path.order } element={ <SellerOrders /> } />
-        <Route path={ path.admin } element={ <AdminManager /> } />
-        <Route exact path="/" element={ <Navigate to={ path.login } /> } />
-      </Routes>
+      <Routers />
       <footer>© Ostron company Todos direitos reservados</footer>
     </div>
   );

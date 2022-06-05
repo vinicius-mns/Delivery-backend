4627d393f16e745eb6ae27eef41f1f622864ceaa
vinicius silva <vinicius_dev@hotmail.com>
2022-06-05 19:28:28 -0300

    feat(front): Cria utils para rotas

---
+++ front-end/src/utils/paths.js
@@ -0,0 +1,5 @@
+export const login = '/login';
+export const registe = '/register';
+export const product = '/customer/products/';
+export const order = '/seller/orders';
+export const admin = '/admin/manage'

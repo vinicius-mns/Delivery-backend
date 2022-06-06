315c0c03327f8e931b6c2207c4dc276612210b43
HajimeSonkai <mfilho86@gmail.com>
2022-06-06 15:55:43 -0300

    Feat(front): add rota de checkout

--- front-end/src/component/Routers.jsx
+++ front-end/src/component/Routers.jsx
@@ -8,12 +8,14 @@ import Products from '../pages/Products';
 import Register from '../pages/Register';
 import SellerOrders from '../pages/SellerOrders';
 import AdminManager from '../pages/AdminManager';
+import Checkout from '../pages/Checkout';
 
 const Routers = () => (
   <Routes>
     <Route path={ path.login } element={ <Login /> } />
     <Route path={ path.register } element={ <Register /> } />
     <Route path={ path.product } element={ <Products /> } />
+    <Route path={ path.checkout } element={ <Checkout /> } />
     <Route path={ path.order } element={ <SellerOrders /> } />
     <Route path={ path.admin } element={ <AdminManager /> } />
     <Route exact path="/" element={ <Navigate to={ path.login } /> } />

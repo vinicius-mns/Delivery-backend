68cd0eb49fe1ad1c05a015157731d9e30673a546
Leandro <leandrooliveira35@gmail.com>
2022-06-07 18:44:47 -0300

    feat(back):cria funções de createSale, getSaleId, getByUserId, getBySellerId no service de sale

---
+++ back-end/src/services/saleService.js
@@ -0,0 +1,40 @@
+const { Sales, SalesProducts } = require('../database/models');
+
+const createSale = async (dataSale) => {
+    const { cart, totalPrice, deliveryAddress, deliveryNumber, sellerId, userId } = dataSale;
+    const status = 'pending';
+    const saleDate = new Date();
+    const sale = await Sales
+      .create({ userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, saleDate });
+    cart.forEach((element) => {
+      const { id, quantity } = element;
+      SalesProducts.create({ productId: id, quantity, saleId: sale.dataValues.id });
+    });
+    return sale.dataValues.id;
+};
+
+const getBySaleId = async (id) => {
+  const sale = await Sales.findOne({
+    where: { id },
+    include: { model: SalesProducts }, 
+  });
+  return sale;
+};
+
+const getByUserId = async (userId) => {
+  const sales = await Sales.findAll({
+    where: { userId },
+    include: { model: SalesProducts }, 
+  });
+  return sales;
+};
+
+const getBySellerId = async (sellerId) => {
+  const sales = await Sales.findAll({
+    where: { sellerId },
+    include: { model: SalesProducts }, 
+  });
+  return sales;
+};
+
+module.exports = { getBySellerId, getByUserId, createSale, getBySaleId };

f2f635099b8a3fd9ed174b4f1a0d823817c3dc24
vinicius silva <vinicius_dev@hotmail.com>
2022-06-08 03:39:19 -0300

    feat(front):  Muda o component `CardOrder` para button

--- front-end/src/component/CardOrder.jsx
+++ front-end/src/component/CardOrder.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const CardOrder = ({ order, status }) => (
-  <div className="cardOrder grey">
+  <button type="button" className="cardOrder grey">
     <div>
       <span>Pedido</span>
       <span>{order}</span>
@@ -14,7 +14,7 @@ const CardOrder = ({ order, status }) => (
       <span>03/06/22</span>
       <span>R$: 29,55</span>
     </div>
-  </div>
+  </button>
 );
 
 CardOrder.propTypes = {

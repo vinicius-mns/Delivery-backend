8884e04689f5824a796800d7a1814999c3db5abe
Jesse Benevides <jessebenevides@gmail.com>
2022-06-08 19:10:57 -0300

    feat(front): implementa OrderDetails Page

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,10 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import SubCard from '../component/SubCard';
 import { requestGet, setToken } from '../service/request';
 
 const OrderDetails = () => {
   const [order, setOrder] = useState({});
+  const [productList, setProductList] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   const { id } = useParams();
+  const prefix = 'customer_order_details__';
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const allProducts = await requestGet('/products');
+      setProductList(allProducts);
+    };
+
+    getProducts();
+  }, []);
 
   useEffect(() => {
     setToken(JSON.parse(localStorage.getItem('user')).token);
@@ -16,11 +30,80 @@ const OrderDetails = () => {
     getOrder();
   }, [id]);
 
-  console.log(order);
+  useEffect(() => {
+    const filterProducts = () => {
+      const { SalesProducts } = order;
+      const filteredProductsList = SalesProducts && SalesProducts.map((saleDetails) => {
+        const data = productList.find((product) => product.id === saleDetails.productId);
+        const { quantity } = saleDetails;
+        return ({ quantity, ...data });
+      });
+      setFilteredProducts(filteredProductsList);
+    };
+    if (order) filterProducts();
+  }, [order, productList]);
+
   return (
     <div>
-      <h2>{id}</h2>
-      <p>Oi</p>
+      <div>
+        <span
+          data-testid={ `${prefix}element-order-details-label-order-id` }
+        >
+          {`Pedido 000${order.id}`}
+        </span>
+        <span
+          data-testid={ `${prefix}element-order-details-label-seller-name` }
+        >
+          P. Vend: Fulana Pereira
+        </span>
+        <span
+          data-testid={ `${prefix}element-order-details-label-order-date` }
+        >
+          { order.saleDate }
+        </span>
+        <span
+          data-testid={ `${prefix}element-order-details-label-delivery-status` }
+        >
+          { order.status }
+        </span>
+        <button
+          type="button"
+          data-testid={ `${prefix}button-delivery-check` }
+        >
+          MARCAR COMO ENTREGUE
+        </button>
+      </div>
+      <div>
+        <div className="containerSubCard">
+          <div className="subCard">
+            <span className="a">Item</span>
+            <span className="b">Descrição</span>
+            <span className="c">Quantidade</span>
+            <span className="d">Valor Unitario</span>
+            <span className="e">Sub-Total</span>
+          </div>
+        </div>
+        {
+          filteredProducts
+            && filteredProducts.map(({ id: productId, name, price, quantity }, index) => (
+              <SubCard
+                key={ productId }
+                id={ index }
+                name={ name }
+                quantity={ quantity }
+                valueU={ price.toString().replace('.', ',') }
+                sub={ (price * quantity).toFixed(2).replace('.', ',') }
+                pr="order_details"
+              />
+            ))
+        }
+        <div className="totalPrice">
+          <span>{'Total R$: '}</span>
+          <span data-testid={ `${prefix}element-order-total-price` }>
+            { order.totalPrice.toFixed(2).replace('.', ',') }
+          </span>
+        </div>
+      </div>
     </div>
   );
 };

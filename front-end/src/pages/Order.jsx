3bf812f33901cd9b52d30a247342e9709ae71118
vinicius silva <vinicius_dev@hotmail.com>
2022-06-08 14:38:02 -0300

    feat(front):  Pequen refatoracao no compoenent `CardOrder`

--- front-end/src/pages/Order.jsx
+++ front-end/src/pages/Order.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import CardOrder from '../component/CardOrder';
 import '../styles/order.css';
 
+const pedidos = [
+  { order: 1, status: 'Pendente' },
+  { order: 2, status: 'Pendente' },
+  { order: 3, status: 'Preparando' },
+  { order: 4, status: 'Entregue' },
+  { order: 5, status: 'Entregue' },
+  { order: 6, status: 'Entregue' },
+  { order: 7, status: 'Entregue' },
+];
+
+// const coisa = (event) => {
+//   event.currentTarget.classList.toggle('fullSize');
+// };
+
 const Order = () => (
   <div className="order">
     <h2>Meus pedidos</h2>
     <div className="orderContainer">
-      <CardOrder order="0001" status="Pendente" />
-      <CardOrder order="0003" status="Preparando" />
-      <CardOrder order="0004" status="Entregue" />
-      <CardOrder order="0005" status="Entregue" />
-      <CardOrder order="0006" status="Entregue" />
-      <CardOrder order="0007" status="Entregue" />
-      <CardOrder order="0008" status="Entregue" />
-      <CardOrder order="0009" status="Entregue" />
-      <CardOrder order="0010" status="Entregue" />
-      <CardOrder order="0021" status="Entregue" />
+      {
+        pedidos.map(({ order, status }, i) => (
+          <CardOrder order={ order } status={ status } key={ i } />
+        ))
+      }
     </div>
   </div>
 );

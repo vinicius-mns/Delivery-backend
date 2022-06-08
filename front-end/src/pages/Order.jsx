1d99c8f76f3782cf639fbe8d35e94e759f4e9c70
Jesse Benevides <jessebenevides@gmail.com>
2022-06-08 20:43:59 -0300

    refactor(front): add requisição das order no DB

--- front-end/src/pages/Order.jsx
+++ front-end/src/pages/Order.jsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardOrder from '../component/CardOrder';
+import { requestGet, setToken } from '../service/request';
 import '../styles/order.css';
 
-const pedidos = [
-  { order: 1, status: 'Pendente' },
-  { order: 2, status: 'Pendente' },
-  { order: 3, status: 'Preparando' },
-  { order: 4, status: 'Entregue' },
-  { order: 5, status: 'Entregue' },
-  { order: 6, status: 'Entregue' },
-  { order: 7, status: 'Entregue' },
-];
+// const pedidos = [
+//   { order: 1, status: 'Pendente', totalPrice, saleDate },
+// ];
 
 // const coisa = (event) => {
 //   event.currentTarget.classList.toggle('fullSize');
 // };
 
-const Order = () => (
-  <div className="order">
-    <h2>Meus pedidos</h2>
-    <div className="orderContainer">
-      {
-        pedidos.map(({ order, status }, i) => (
-          <CardOrder order={ order } status={ status } key={ i } />
-        ))
-      }
+const Order = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    setToken(JSON.parse(localStorage.getItem('user')).token);
+    const getOrders = async () => {
+      const allOrders = await requestGet('/sales');
+      setOrders(allOrders);
+    };
+
+    getOrders();
+  }, []);
+
+  return (
+    <div className="order">
+      <h2>Meus pedidos</h2>
+      <div className="orderContainer">
+        { console.log(orders) }
+        {
+          orders?.map(({ id, status, saleDate, totalPrice }, i) => (
+            <CardOrder
+              order={ id }
+              status={ status }
+              key={ i }
+              date={ saleDate }
+              price={ totalPrice }
+            />
+          ))
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Order;

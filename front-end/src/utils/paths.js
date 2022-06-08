a54f139bc0820296e4000501bc1d75bd698314e6
Jesse Benevides <jessebenevides@gmail.com>
2022-06-08 17:16:58 -0300

    fix(front): ajusta paths em /orders

--- front-end/src/utils/paths.js
+++ front-end/src/utils/paths.js
@@ -4,5 +4,5 @@ export const product = '/customer/products/';
 export const checkout = '/customer/checkout';
 export const order = '/seller/orders';
 export const admin = '/admin/manage';
-export const orderCustomer = '/customer/order';
-export const orderDetails = '/customer/order/:id';
+export const orderCustomer = '/customer/orders';
+export const orderDetails = '/customer/orders/:id';

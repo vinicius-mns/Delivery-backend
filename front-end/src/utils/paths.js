87bf5e65c5a50473f9823917ddeff95751aad278
Arthur Bernardo Messias <turmessias@gmail.com>
2022-06-08 16:47:18 -0300

    feat(front) - adiciona novo path

--- front-end/src/utils/paths.js
+++ front-end/src/utils/paths.js
@@ -4,4 +4,5 @@ export const product = '/customer/products/';
 export const checkout = '/customer/checkout';
 export const order = '/seller/orders';
 export const admin = '/admin/manage';
-export const orderCustomer = '/customer/order/:id';
+export const orderCustomer = '/customer/order';
+export const orderDetails = '/customer/order/:id';

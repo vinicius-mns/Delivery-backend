fc41eff097854bc87759824cad355f60b5d4bad9
Arthur Bernardo Messias <turmessias@gmail.com>
2022-06-09 17:09:05 -0300

    feat(front) - cria componente SellerOrders

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import CardOrder from '../component/CardOrder';
+import CustomerContext from '../context/CustomerContext';
+import { requestGet, setToken } from '../service/request';
+import Bar from '../component/Bar';
+// import '../styles/order.css';
 
-const SellerOrders = () => (
-  <div>
-    <h1>Toma-lhe teu produto vendedor!</h1>
-  </div>
-);
+// const pedidos = [
+//   { order: 1, status: 'Pendente', totalPrice, saleDate },
+// ];
 
-export default SellerOrders;
+// const coisa = (event) => {
+//   event.currentTarget.classList.toggle('fullSize');
+// };
+
+const SellerOrder = () => {
+  const { orders, setOrders } = useContext(CustomerContext);
+
+  useEffect(() => {
+    setToken(JSON.parse(localStorage.getItem('user')).token);
+    const getOrders = async () => {
+      const allOrders = await requestGet('/sales');
+      setOrders(allOrders);
+    };
+
+    getOrders();
+  }, [setOrders]);
+
+  return (
+    <div className="order">
+      <Bar roles />
+      <h2>Meus pedidos</h2>
+      <div className="orderContainer">
+        {
+          orders?.map(({ id, status, saleDate, totalPrice }, i) => (
+            <CardOrder
+              order={ id }
+              status={ status }
+              key={ i }
+              date={ saleDate }
+              price={ totalPrice }
+              roles="seller"
+            />
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default SellerOrder;

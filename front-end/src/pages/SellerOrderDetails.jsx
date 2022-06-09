e56b03831a52801884d658081a2ab15e31d23d9a
Arthur Bernardo Messias <turmessias@gmail.com>
2022-06-09 17:08:31 -0300

    feat(front) - cria componente SellerOrderDetails

---
+++ front-end/src/pages/SellerOrderDetails.jsx
@@ -0,0 +1,127 @@
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import SubCard from '../component/SubCard';
+import { requestGet, setToken } from '../service/request';
+import Bar from '../component/Bar';
+
+const SellerOrderDetails = () => {
+  const [order, setOrder] = useState({});
+  const [productList, setProductList] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  // const [disabled, setDisabled] = useState(true);
+
+  const { id } = useParams();
+  const prefix = 'seller_order_details__';
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const allProducts = await requestGet('/products');
+      setProductList(allProducts);
+    };
+
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    setToken(JSON.parse(localStorage.getItem('user')).token);
+    const getOrder = async () => {
+      const data = await requestGet(`/sales/${id}`);
+      setOrder(data);
+    };
+
+    getOrder();
+  }, [id]);
+
+  useEffect(() => {
+    const filterProducts = () => {
+      const { SalesProducts } = order;
+      const filteredProductsList = SalesProducts && SalesProducts.map((saleDetails) => {
+        const data = productList.find((product) => product.id === saleDetails.productId);
+        const { quantity } = saleDetails;
+        return ({ quantity, ...data });
+      });
+      setFilteredProducts(filteredProductsList);
+    };
+    if (order) filterProducts();
+  }, [order, productList]);
+
+  // useEffect(() => {
+  //   console.log(order);
+  //   if (order) {
+  //     return setDisabled(false);
+  //   }
+
+  //   return setDisabled(true);
+  // }, [order]);
+
+  return (
+    <div>
+      <Bar roles />
+      <div>
+        <span
+          data-testid={ `${prefix}element-order-details-label-order-id` }
+        >
+          {`Pedido 000${order && order.id}`}
+        </span>
+        <span
+          data-testid={ `${prefix}element-order-details-label-order-date` }
+        >
+          { moment(order && order.saleDate).format('DD/MM/YYYY') }
+        </span>
+        <span
+          data-testid={ `${prefix}element-order-details-label-delivery-status` }
+        >
+          { order && order.status }
+        </span>
+        <button
+          type="button"
+          data-testid={ `${prefix}button-preparing-check` }
+        >
+          PREPARAR PEDIDO
+        </button>
+        <button
+          type="button"
+          disabled
+          data-testid={ `${prefix}button-dispatch-check` }
+        >
+          SAIU PARA ENTREGA
+        </button>
+      </div>
+      <div>
+        <div className="containerSubCard">
+          <div className="subCard">
+            <span className="a">Item</span>
+            <span className="b">Descrição</span>
+            <span className="c">Quantidade</span>
+            <span className="d">Valor Unitario</span>
+            <span className="e">Sub-Total</span>
+          </div>
+        </div>
+        {
+          filteredProducts
+            && filteredProducts.map(({ id: productId, name, price, quantity }, index) => (
+              <SubCard
+                key={ productId }
+                id={ index }
+                name={ name }
+                quantity={ quantity }
+                valueU={ price.toString().replace('.', ',') }
+                sub={ (price * quantity).toFixed(2).replace('.', ',') }
+                pr="order_details"
+              />
+            ))
+        }
+        <div className="totalPrice">
+          <span>{'Total R$: '}</span>
+          <span data-testid={ `${prefix}element-order-total-price` }>
+            { order && typeof order.totalPrice === 'string'
+              && order.totalPrice.replace('.', ',') }
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SellerOrderDetails;

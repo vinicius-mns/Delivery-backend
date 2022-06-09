d700404dee500c214ffee0e781d7eb360f7cb729
Arthur Bernardo Messias <turmessias@gmail.com>
2022-06-09 18:35:50 -0300

    feat(back) - add rota edit

--- back-end/src/routes/salesRoute.js
+++ back-end/src/routes/salesRoute.js
@@ -7,5 +7,6 @@ const salesRouter = express.Router();
 salesRouter.post('/', authMiddleware, saleController.createSale);
 salesRouter.get('/', authMiddleware, saleController.getByUser);
 salesRouter.get('/:id', authMiddleware, saleController.getBySaleId);
+salesRouter.put('/:id', authMiddleware, saleController.editStatus);
 
 module.exports = { salesRouter };
